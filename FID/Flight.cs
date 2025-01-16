4e929c8b67603598ce3df599081bf56b36f7b3ab
King-Dhush <S10270619@connect.np.edu.sg>
2025-01-16 18:23:18 +0800

    Established Flight.cs
    Established the Flight class

--- FID/Flight.cs
+++ FID/Flight.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FID
+//==========================================================
+// Student Number : S10270619
+// Student Name : Dhushyanth
+// Partner Name : Hafiz
+//==========================================================
+﻿using System.Collections.Generic;
+public abstract class Flight
 {
-    internal class Terminal
+    public string FlightNumber { get; set; }
+    public string Origin { get; set; }
+    public string Destination { get; set; }
+    public DateTime ExpectedTime { get; set; }
+    public string Status { get; set; }
+
+    public Flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status = "Scheduled")
     {
+        FlightNumber = flightNumber;
+        Origin = origin;
+        Destination = destination;
+        ExpectedTime = expectedTime;
+        Status = status;
+    }
+
+    public abstract double CalculateFees();
+
+    public override string ToString()
+    {
+        return $"{FlightNumber}: {Origin} -> {Destination}, Time: {ExpectedTime}, Status: {Status}";
     }
 }
