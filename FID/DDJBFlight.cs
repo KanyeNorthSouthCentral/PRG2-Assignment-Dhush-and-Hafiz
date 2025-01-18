6c1614ed929bd8c5af4dd582e6471613864572f6
Muhammad Hafiz Bin Mohamed Noor <s10270616@connect.np.edu.sg>
2025-01-18 15:20:44 +0800

    Created the menu display in program.cs
    done by Muhammad Hafiz

--- FID/DDJBFlight.cs
+++ FID/DDJBFlight.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FID
-{
-    internal class Terminal
-    {
-    }
-}
+//namespace FID
+//{
+//    internal class DDJBFlight : Flight
+//    {
+        
+    
+//    }
+//}
--- FID/Program.cs
+++ FID/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void DisplayMenu()
+{
+    Console.WriteLine("=============================================");
+    Console.WriteLine("Welcome to Changi Airport Terminal 5");
+    Console.WriteLine("=============================================");
+
+    Console.WriteLine("1. List All Flights\n" +
+        "2. List Boarding Gates\n" +
+        "3. Assign a Boarding Gate to a Flight\n" +
+        "4. Create Flight\n" +
+        "5. Display Airline Flights\n" +
+        "6. Modify Flight Details\n" +
+        "7. Display Flight Schedule\n" +
+        "0. Exit");
+}
+
+DisplayMenu();
